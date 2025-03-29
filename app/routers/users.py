7165262295a66eee212d427a2ae8479b49296669
jaynamm <polky0421@gmail.com>
2025-03-30 00:00:10 +0900

    feat: path parameter

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/users", tags=["user"])
+router = APIRouter(tags=["user"])
 
 
-@router.get("/")
+@router.get("/user")
 async def get_users():
     return [
         {
@@ -17,8 +17,33 @@ async def get_users():
     ]
 
 
-@router.get("/{id}")
-async def get_users(id: str):
+@router.get("/user/me")
+async def get_users():
+    return {
+        "name": "jaynam",
+        "position": "data engineer",
+    }
+
+
+from app.schemas.user_schema import UserInfoType
+
+
+@router.get("/user/info/{type}")
+async def get_user_info(type: UserInfoType):
+    if type == UserInfoType.name:
+        return {
+            "type": type,
+            "name": "jaynam",
+        }
+    if type.value == "position":
+        return {
+            "type": type,
+            "position": "data engineer",
+        }
+
+
+@router.get("/user/{id}")
+async def get_users(id: int):
     if id == "1":
         return {
             "name": "jaynam",
---
+++ app/schemas/user_schema.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class UserInfoType(str, Enum):
+    name = "name"
+    position = "position"
